4453a53212d485d49a248f3207cf30a47712e6cd
JZims <zimmerman.j@hotmail.com>
2023-12-06 10:31:58 -0500

    etherium library configured for walletconnect

--- frontend/nft-brawl-fe/src/App.tsx
+++ frontend/nft-brawl-fe/src/App.tsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'viem/chains'
+
+const projectId = 'YOUR_PROJECT_ID'
+
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
 
 function App() {
   const [count, setCount] = useState(0)
